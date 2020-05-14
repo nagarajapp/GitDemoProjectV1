1092710d4e44af26e57780adc956b6b1aa287422
nagaraj <nagaraj@appfire.com>
2020-05-14 11:16:49 +0530

    First commit

---
+++ src/GitDemo/GitTest1.java
@@ -0,0 +1,26 @@
+package GitDemo;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hungry = "Yes";
+		
+	if (hungry== "Yes") 
+	System.out.println("Eat Somthing");
+	
+	else {
+		System.out.println("Do something");
+	}
+	
+		
+	}
+	
+		}
+		
+	
+
+
+
